ed5c7ff58f25f98bcf05f45f67224276e3c7bf6e
Mikaels Slava <mikaels.slava@gmail.com>
2020-10-24 17:20:21 +0300

    first commit, basic environment with working React and SCSS

---
+++ src/components/PageContent/PageContent.js
@@ -0,0 +1,11 @@
+import './PageContent.scss';
+
+function PageContent() {
+  return (
+    <div className="PageContent">
+      <p>Page Content gets rendered here</p>
+    </div>
+  );
+}
+
+export default PageContent;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.scss';
+import PageContent from './components/PageContent/PageContent';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PageContent />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
