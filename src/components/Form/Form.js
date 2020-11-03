b03f605ac7f8f6b9bc51293a51e392eecbd2b43b
Mikaels Slava <mikaels.slava@gmail.com>
2020-11-03 15:14:41 +0200

    TASK1
    prepared Form.js for TASK2

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,18 +3,38 @@ import './Form.scss';
 
 class Form extends React.Component {
 
-  submitForm() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      termsOfService: false
+     };
+  }
+
+  handleClick = (event) => {
     // insert TASK2 functionality here
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  }
+
+  handleCheckboxChange = (event) => {
+    this.setState({ termsOfService: event.target.checked });
+  }
+
+  componentDidUpdate() {
+    document.getElementById("form-submit").addEventListener("click", this.handleClick);
   }
 
   render() {
     return (
-        <form className="Form" onSubmit={this.handleSubmit}>
+        <form id="newsletter-form" className="Form">
           <div className="Form-EmailInputWrapper">
-            <input className="Form-EmailInput" id="email" type="email" name="email" placeholder="Type your email address here..." required/>
-            <div className="Form-EmailInputWrapperArrow" onClick= { this.submitForm() }> </div>
+            <input className="Form-EmailInput" id="email" type="email" name="email" placeholder="Type your email address here..." onChange={ this.handleEmailChange } required/>
+            <div id="form-submit" className="Form-EmailInputWrapperArrow"> </div>
           </div>
-          <input className="Form-Checkbox" type="checkbox" id="termsOfService" />
+          <input className="Form-Checkbox" type="checkbox" id="termsOfService" onChange={ this.handleCheckboxChange }/>
           <div className="Form-CheckboxLabel" for="checkbox">
             <p>I agree to <a>terms of service</a></p>
           </div>
